e70d435aa44a427955ff7158092352f1dceab84c
Valentin <33943799+valen20Chx@users.noreply.github.com>
2020-02-16 01:55:10 +0100

    Client - Server Communication

--- public/js/partie.js
+++ public/js/partie.js
@@ -1,71 +1,72 @@
-var socket = io();
+let socket = io('/host');
 
-var gameContainer = document.getElementById('game-container');
-var initButton = document.getElementById('init-button');
-var initContainer = document.getElementById('init-container');
-var loadingContainer = document.getElementById('loading-container');
+let gameContainer = document.getElementById('game-container');
+let initButton = document.getElementById('init-button');
+let initContainer = document.getElementById('init-container');
+let loadingContainer = document.getElementById('loading-container');
 
 const game_states = {
     NONE: "none",
     LOBBY: "lobby",
-    PLAYING: "playing"
+    PLAYING: "playing",
+    VOTING: "voting"
 };
 
 const MAX_PLAYER = 8; // CAN CHANGE
 
-var player_list = [];
+let player_list = [];
 
-var game_state = game_states.NONE;
+let game_state = game_states.NONE;
 
-var roomCode = null;
+let roomCode = null;
 
 // Server calls
 socket.on('connect', () => {
     console.log(socket);
     connectionEstablished();
 });
+
 socket.on('init', init_funct);
+
 socket.on('add-player', (args) => {
     add_player(args);
 });
 
-initButton.onclick = () => {
-    // TODO: Request Game Code
-    socket.emit('getRoomCode');
-};
+socket.on('startGame', startGame);
 
-socket.on('getRoomCodeRes', (args) => { // TODO: Do serverSide
-    roomCode = args.roomCode;
-    init_funct({
-        roomCode: args.roomCode
+initButton.onclick = () => {
+    socket.emit('getRoomCode');
+
+    socket.on('game-launched', (args) => {
+        roomCode = args.roomCode;
     });
-});
+};
 
 function init_funct(args) {
     // INIT GAME AND ENTER LOBBY
-    var roomCode = args.roomCode;
+    let roomCode = args.roomCode;
     game_state = game_states.LOBBY;
 
     gameContainer.style.display = 'inline';
     initContainer.style.display = 'none';
 
-    var roomCodeEle = document.createElement('p');
+    let roomCodeEle = document.createElement('p');
     roomCodeEle.classList.add('roomCode');
     roomCodeEle.innerText = roomCode;
     gameContainer.append(roomCodeEle);
 
-    var player_list_ele = document.createElement('li');
+    let player_list_ele = document.createElement('li');
     player_list_ele.classList.add('player-list');
     for(let i = 0; i < MAX_PLAYER; i++) {
         // Create Empty Slot
-        var player_ele = document.createElement('div');
+        let player_ele = document.createElement('div');
 
-        var player_image_ele = document.createElement('img');
+        let player_image_ele = document.createElement('img');
         player_image_ele.classList.add('player-img');
         player_image_ele.src = '/res/img/Empty-Player_QM.png';
         player_image_ele.style.background = `radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(50,50,50,1) 100%)`;
 
-        var player_name_ele = document.createElement('p');
+        let player_name_ele = document.createElement('p');
         player_name_ele.innerText = "Waiting...";
         player_name_ele.classList.add('player-name');
 
@@ -82,8 +83,8 @@ function add_player(player) {
         if(player_list.length < MAX_PLAYER) { // Correct amount of player
             player_list.push(player);
 
-            var player_img_ele = document.getElementsByClassName('player-img').item(player_list.length - 1);
-            var player_name_ele = document.getElementsByClassName('player-name').item(player_list.length - 1);
+            let player_img_ele = document.getElementsByClassName('player-img').item(player_list.length - 1);
+            let player_name_ele = document.getElementsByClassName('player-name').item(player_list.length - 1);
 
             player_name_ele.innerText = player.name;
             player_img_ele.style.background = `radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(${(150 * ((player_list.length - 1) + 1) + ((player_list.length - 1) * 33)) % 256},${(200 * ((player_list.length - 1) + 1) + ((player_list.length - 1) * 33)) % 256},${(255 * ((player_list.length - 1) + 1) + ((player_list.length - 1) * 33)) % 256},1) 100%)`;
@@ -102,7 +103,7 @@ function startGame() {
 
         // Start Game
         // TODO: Explications
-        socket.emit('getTheme', {roomCode: roomCode});
+        socket.emit('getTheme');
 
     } else {
         console.log('Error startGame: Can\'t start game when not in lobby');
@@ -111,58 +112,100 @@ function startGame() {
 
 socket.on('getThemeRes', (args) => {
     clearGameContainer();
-    var stageTxt = document.createElement('p')
+    let stageTxt = document.createElement('p')
     stageTxt.classList.add('gmae-stage');
     stageTxt.innerText = 'Stage ' + args.stage;
     gameContainer.append(stageTxt);
 
-    var themeTxt = document.createElement('p');
+    let themeTxt = document.createElement('p');
     themeTxt.classList.add('theme-name'); // TODO : ADD in stylesheet
     themeTxt.innerText = args.theme;
     gameContainer.append(themeTxt);
     
-    var hintTxt = document.createElement('p');
+    let hintTxt = document.createElement('p');
     hintTxt.classList.add('theme-hint'); // TODO : ADD in stylesheet
     hintTxt.innerText = 'Envoyez des photos ayant un rapport avec le theme.';
     gameContainer.append(hintTxt);
 
-    socket.emit('themeDisplayed', {roomCode: roomCode});
+    socket.emit('themeDisplayed');
 });
 
-socket.on('photoUploadCompleted', (args) => {
+socket.on('photoUploadCompleted', () => {
     clearGameContainer();
     // Redistribution des photos (ServerSide)
-    var consigneEle = document.createElement('p');
+    let consigneEle = document.createElement('p');
     consigneEle.classList.add('consigne');
     consigneEle.innerText = 'Rearengez les photos recu pour faire une histoire';
     gameContainer.append(consigneEle);
 
-    socket.emit('consigneDisplayed', {roomCode: roomCode});
+    socket.emit('consigneDisplayed');
 });
 
-
-// https://stackoverflow.com/questions/26331787/socket-io-node-js-simple-example-to-send-image-files-from-server-to-client
 socket.on('presentation', (args) => {
     clearGameContainer();
 
-    var playerNameEle = document.createElement('p');
-    playerNameEle.classList.add('presentation-player-name');
+    let playerNameEle = document.createElement('p');
+    playerNameEle.classList.add('story-player-name');
     playerNameEle.innerText = args.playerName;
     gameContainer.append(playerNameEle);
 
-    var photoArray = [];
-    for(let i = 0; i < args.photos; i++) {
-        let tempCanvas = document.createElement('canvas');
-        let tempCavasCtx = tempCanvas.getContext('2d');
-        let tempPhoto = new Image();
-        tempPhoto.src = 'data:image/jpeg;base64,' + args.photos[i].buffer;
-        tempCavasCtx.drawImage(tempPhoto, 0, 0);
+    const presDelay = 1000;
 
-        photoArray.push(tempCanvas);
+    window.setTimeout(() => {
+        for(let i = 0; i < args.photos; i++) {
+            let tempImg = document.createElement('img');
+    
+            tempImg.src = args.images[i]; // Add image data
+    
+            tempImg.classList.add('story-img');
+    
+            gameContainer.append(tempImg);
+        }
+    }, presDelay);
 
-        gameContainer.append(photoArray[i]);
-    }
 });
+
+socket.on('end-presentation', () => {
+    clearGameContainer();
+});
+
+socket.on('vote-screen', () => {
+    clearGameContainer();
+    game_state = game_states.VOTING;
+
+    const voteTime = 60 * 1000;
+
+    let voteTitle = document.createElement('p');
+    voteTitle.classList.add('vote-title');
+    voteTitle.innerText('Votez pour la meilleur histoire');
+
+    let countdownEle = document.createElement('p');
+    countdownEle.classList.add('vote-countdown');
+
+    let timeCounter = 0;
+    let countDownId = window.setInterval(() => {
+
+        countdownEle.innerText = Math.floor((voteTime - (timeCounter * 1000)) / 1000);
+
+        timeCounter++;
+        if((timeCounter * 1000) > voteTime) {
+            window.clearInterval(countDownId); // Removes Countdown
+        }
+    }, 1000);
+
+    socket.on('vote-finished', () => {
+        window.clearInterval(countDownId); // Removes Countdown
+    });
+    
+    socket.on('player-vote', (data) => {
+        if(game_state == game_states.VOTING) {
+            // TODO : Player as voted
+        } else {
+            // ERROR
+        }
+    });
+});
+
 
 // TODO : Continue Here
 
@@ -185,7 +228,7 @@ function endGame() {
     gameContainer.style.display = 'none';
     loadingContainer.style.display = 'none';
     initContainer.style.display = 'none';
-    var gameOverParagraph = document.createElement('p');
+    let gameOverParagraph = document.createElement('p');
     gameOverParagraph.innerText = "Game Over.";
     gameOverParagraph.style.fontSize = "50px";
     gameOverParagraph.style.fontWeight = "bold";
